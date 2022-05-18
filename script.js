ae78bddedd7b84b28cc467207222b46786414a36
dreessen-n <dreessen.edu@gmail.com>
2022-05-17 18:37:55 -0600

    Update the changeTemp function from a static load to a calculating temps. Temp are slightly off due to rounding

--- script.js
+++ script.js
@@ -12,23 +12,56 @@ function loadWeather() {
 
 // Create 3 arrary to store temps and id values
 // write function to change temps when button value changes
+// static version - Robert said to rewrite to calculate temps!
 
 
-var cel = ['24&deg;', '18&deg;', '27&deg;', '19&deg;', '21&deg;', '16&deg;', '26&deg;', '21&deg;'];
+// var cel = ['24&deg;', '18&deg;', '27&deg;', '19&deg;', '21&deg;', '16&deg;', '26&deg;', '21&deg;'];
 
-var far = ['75&deg;', '65&deg;', '80&deg;', '66&deg;', '69&deg;', '61&deg;', '78&deg;', '70&deg;'];
+// var far = ['75&deg;', '65&deg;', '80&deg;', '66&deg;', '69&deg;', '61&deg;', '78&deg;', '70&deg;'];
 
-var tempIds = ['h1', 'l1', 'h2', 'l2', 'h3', 'l3', 'h4', 'l4'];
+// var tempIds = ['h1', 'l1', 'h2', 'l2', 'h3', 'l3', 'h4', 'l4'];
 
+
+// function changeTemps(element) {
+//     if (element.value == 'f') {
+//         for (var i = 0; i < 8; i++) {
+//             document.getElementById(tempIds[i]).innerHTML = far[i];
+//         }
+//     } else {
+//         for (var i = 0; i < 8; i++) {
+//             document.getElementById(tempIds[i]).innerHTML = cel[i];
+//         }
+//     }
+// }
+
+// dynamic version of temp function
+
+function calcF(C) {
+    var F = ((9 * C) / 5) + 32;
+    return Math.round(F);
+}
+
+function calcC(F) {
+    var C = (5 * (F - 32)) /9;
+    return Math.round(C);
+}
 
 function changeTemps(element) {
     if (element.value == 'f') {
+        // convert to Fahrenheit
         for (var i = 0; i < 8; i++) {
-            document.getElementById(tempIds[i]).innerHTML = far[i];
+            var x = document.getElementById('t' + [i]);
+            console.log(x);
+            var y = parseInt(x.innerHTML);
+            console.log(y);
+            x.innerHTML = calcF(y);
         }
     } else {
         for (var i = 0; i < 8; i++) {
-            document.getElementById(tempIds[i]).innerHTML = cel[i];
+            // convert to Celsius
+            var x = document.getElementById('t' + [i]);
+            var y = parseInt(x.innerHTML);
+            x.innerHTML = calcC(y);
         }
     }
 }
